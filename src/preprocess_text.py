9f5fb7718412f11e0eacead49581b04464a207c3
jmoschos <moschos.vangelis@gmail.com>
2025-01-08 13:53:59 +0100

    added stopword removal

--- src/preprocess_text.py
+++ src/preprocess_text.py
@@ -1,11 +1,14 @@
 import re
 import string
 from textblob import TextBlob
+from nltk.corpus import stopwords
+import nltk
 
 class DataPreprocessor:
     def __init__(self, df):
         self.df = df
         self.punctuation = string.punctuation
+        nltk.download('stopwords')
 
     def make_lower_case(self, col):
         self.df[col] = self.df[col].str.lower()
@@ -27,10 +30,21 @@ class DataPreprocessor:
         textBlb = TextBlob(text)
         return textBlb.correct().string
 
+    @staticmethod
+    def remove_stopwords(text):
+        new_text = []
+        for word in text.split():
+            if word in stopwords.words('english'):
+                new_text.append('')
+            else:
+                new_text.append(word)
+        x = new_text[:]
+        new_text.clear()
+        return " ".join(x)
+
     def fix_all_data(self, col):
         self.make_lower_case(col)
         self.df[col] = self.df[col].apply(self.handle_html_tags)
         self.df[col] = self.df[col].apply(self.remove_url)
         self.df[col] = self.df[col].apply(self.remove_punctuation)
-        #self.df[col] = self.df[col].apply(self.fix_typos)
         return self.df
