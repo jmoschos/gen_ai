9f02c1a7e77cabe0228bc3728a02c9f69629c45b
jmoschos <moschos.vangelis@gmail.com>
2025-01-10 15:57:36 +0100

    refactored hf class

--- src/main.py
+++ src/main.py
@@ -3,4 +3,6 @@ from hugging_face import Solution
 
 if __name__ == '__main__':
     #Pipeline().run()
-    Solution()
+    solution = Solution.with_default_model()
+    result = solution.get_sentiment("I think it was the best thing ive ever seen.")
+    print(result)
